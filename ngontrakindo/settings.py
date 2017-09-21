ec2146fc715cb9ac4459fc2e64f75b8fce0d36ef
Dirga Ekaputra <ekaputra.dirga@gmail.com>
2017-09-21 09:50:03 +0700

    ngontrakindo

--- ngontrakindo/settings.py
+++ ngontrakindo/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'ngontrakindo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates"), "\Users\Dirga\Desktop\env\src"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
